46f42f566ec4f7a8a5692956f48003fa1811af94
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2025-05-04 10:02:34 +0200

    finding occurence of a single integer that appears odd times

--- javascript katas/8kyu/findingOddInt.js
+++ javascript katas/8kyu/findingOddInt.js
@@ -2,13 +2,15 @@
 
 // There will always be only one integer that appears an odd number of times
 
-Given an array of integers, find the one that appears an odd number of times.
+//For finding one number that appears odd times
 
-There will always be only one integer that appears an odd number of times.
+function trouverOccurences(num){
+    let x=0; //Initialise x to 0, xoring with 0 does not changge the value
+    for(let i=0; i<num.length;i++){
+        x=x^num[i]  //xor operation will cancel out numbers that appear an even number of times 
+    }
+    return x;
+}
 
-Examples
-[7] should return 7, because it occurs 1 time (which is odd).
-[0] should return 0, because it occurs 1 time (which is odd).
-[1,1,2] should return 2, because it occurs 1 time (which is odd).
-[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
-[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+let number =trouverOccurences([7,7,8,8,4,4,6,6,7,4,4])
+console.log(number);
