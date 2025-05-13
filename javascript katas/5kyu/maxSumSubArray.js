f50f49f01196a2bb8b5c0062fd8398f331a5a93f
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2025-05-13 12:38:16 +0200

    before correction

--- javascript katas/5kyu/maxSumSubArray.js
+++ javascript katas/5kyu/maxSumSubArray.js
@@ -3,6 +3,35 @@ function maxSequence(arr){
     const result =[];
     for (let st=0; st<arr.length; st++){
         let subArray =[];
-        
+        for(let en=0; en<arr.length; en++){
+            subArray.push[arr[en]]
+            result.push([...subArray])
+            
+        }
+        return result;
     }
+    let allSubArrays = maxSequence([1, 2, 3, 4]);
+    function maxSumSubArray(allSubArrays){
+        let counter= 0;
+        for(let i=0; i<allSubArrays.length; i++){
+            let sumArray =[];
+            for (let j=0;j<allSubArrays[i].length; j++){
+                counter += allSubArrays[i][j];
+                sumArray.push(counter);
+            }                
+        }
+        return sumArray
+    }
+    function greatestSum(sumArray){
+        let greatestSum = 0;
+        for(let i=0; i<sumArray.length; i++){
+            if(sumArray[i]>greatestSum){
+                greatestSum = sumArray[i];
+            }
+           
+    }
+        return greatestSum;
+    
+}
+    
 }
