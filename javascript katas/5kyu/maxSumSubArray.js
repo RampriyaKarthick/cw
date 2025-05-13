8b2f6ed4bf6eaf4938ad32bd68ad28072af65341
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2025-05-13 12:50:13 +0200

    correct sln

--- javascript katas/5kyu/maxSumSubArray.js
+++ javascript katas/5kyu/maxSumSubArray.js
@@ -3,35 +3,43 @@ function maxSequence(arr){
     const result =[];
     for (let st=0; st<arr.length; st++){
         let subArray =[];
-        for(let en=0; en<arr.length; en++){
-            subArray.push[arr[en]]
+        for(let en=st; en<arr.length; en++){
+            subArray.push(arr[en])
             result.push([...subArray])
             
         }
-        return result;
+        
     }
-    let allSubArrays = maxSequence([1, 2, 3, 4]);
-    function maxSumSubArray(allSubArrays){
-        let counter= 0;
-        for(let i=0; i<allSubArrays.length; i++){
-            let sumArray =[];
-            for (let j=0;j<allSubArrays[i].length; j++){
-                counter += allSubArrays[i][j];
-                sumArray.push(counter);
-            }                
-        }
-        return sumArray
-    }
-    function greatestSum(sumArray){
-        let greatestSum = 0;
-        for(let i=0; i<sumArray.length; i++){
-            if(sumArray[i]>greatestSum){
-                greatestSum = sumArray[i];
-            }
+    if (result.length === 0) return 0;
+   
+    let maxSum = 0;
+  for (let i = 0; i < result.length; i++) {
+    let sum = result[i].reduce((a, b) => a + b, 0);
+    if (sum > maxSum) maxSum = sum;
+  }
+
+  return maxSum;
+}
+
+    // let allSubArrays = maxSequence([1, 2, 3, 4]);
+    // function maxSumSubArray(allSubArrays){
+    //     let counter= 0;
+    //     for(let i=0; i<allSubArrays.length; i++){
+    //         let sumArray =[];
+    //         for (let j=0;j<allSubArrays[i].length; j++){
+    //             counter += allSubArrays[i][j];
+    //             sumArray.push(counter);
+    //         }                
+    //     }
+    //     return sumArray
+    // }
+    // function greatestSum(sumArray){
+    //     let greatestSum = 0;
+    //     for(let i=0; i<sumArray.length; i++){
+    //         if(sumArray[i]>greatestSum){
+    //             greatestSum = sumArray[i];
+    //         }
            
-    }
-        return greatestSum;
-    
-}
-    
-}
+    // }
+    //     return greatestSum;
+  
